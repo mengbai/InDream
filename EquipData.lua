55c9000a37e3243c3056a21590ffd7acb8a5e01a
mengbai <1170179870@qq.com>
2015-11-19 15:00:07 +0800

    添加两个文件
    装备和myapp

---
+++ EquipData.lua
@@ -0,0 +1,55 @@
+module("EquipData",package.seeall)
+
+
+EquipMent={}
+EquipMent[1]={}
+EquipMent[1][1]={type="wuQi",name="普通铁剑",attack=20,defense=0}
+EquipMent[1][2]={type="wuQi",name="黄金剑",attack=25,defense=0}
+EquipMent[1][3]={type="wuQi",name="金刚剑",attack=30,defense=0}
+EquipMent[1][4]={type="wuQi",name="紫青剑",attack=45,defense=0}
+EquipMent[1][5]={type="wuQi",name="血月剑",attack=75,defense=0}
+
+EquipMent[2]={}
+EquipMent[2][1]={type="touKui",name="草帽",attack=0,defense=10}
+EquipMent[2][2]={type="touKui",name="皮帽",attack=0,defense=15}
+EquipMent[2][3]={type="touKui",name="铁盔",attack=0,defense=20}
+EquipMent[2][4]={type="touKui",name="钢盔",attack=0,defense=30}
+EquipMent[2][5]={type="touKui",name="黄金帽",attack=0,defense=35}
+
+EquipMent[3]={}
+EquipMent[3][1]={type="yiFu",name="普通布衣",attack=0,defense=10}
+EquipMent[3][2]={type="yiFu",name="丝绸纱衣",attack=0,defense=20}
+EquipMent[3][3]={type="yiFu",name="黄金缕衣",attack=0,defense=30}
+EquipMent[3][4]={type="yiFu",name="翡翠战裙",attack=5,defense=35}
+EquipMent[3][5]={type="yiFu",name="荆棘之甲",attack=15,defense=50}
+
+EquipMent[4]={}
+EquipMent[4][1]={type="dun",name="木盾",attack=0,defense=5}
+EquipMent[4][1]={type="dun",name="加罗盾",attack=0,defense=10}
+EquipMent[4][1]={type="dun",name="鬼脸盾",attack=7,defense=15}
+EquipMent[4][1]={type="dun",name="鹰神盾",attack=10,defense=25}
+EquipMent[4][1]={type="dun",name="黄金圣盾",attack=15,defense=45}
+
+EquipMent[5]={}
+EquipMent[5][1]={type="RedMid",name="小红药水",price=35,addFlood=100,introduction="小红药水，可以增加100血量"}
+EquipMent[5][2]={type="RedMid",name="中红药水",price=50,addFlood=200,introduction="中红药水，可以增加200血量"}
+EquipMent[5][3]={type="RedMid",name="大红药水",price=80,addFlood=300,introduction="大红药水，可以增加350血量，让Boss绝望的神奇药水"}
+
+EquipMent[6]={}
+EquipMent[6][1]={type="BlueMid",name="小蓝药水",price=35,addEnergy=100,introduction="小蓝药水，可以增加100点蓝"}
+EquipMent[6][2]={type="BlueMid",name="中蓝药水",price=50,addEnergy=100,introduction="中蓝药水，可以增加250点蓝"}
+EquipMent[6][3]={type="BlueMid",name="巨蓝药水",price=100,addEnergy=100,introduction="巨蓝药水，可以喝饱的蓝药水，增加500点蓝"}
+-- EquipMent[5]={}
+-- EquipMent[5][1]={type="jieZi",name="铜戒",attack=1,defense=10}
+-- EquipMent[5][1]={type="jieZi",name="银戒",attack=5,defense=15}
+-- EquipMent[5][1]={type="jieZi",name="金戒",attack=5,defense=20}
+-- EquipMent[5][1]={type="jieZi",name="黄金扳指",attack=15,defense=30}
+-- EquipMent[5][1]={type="jieZi",name="翠玉戒",attack=20,defense=45}
+
+
+-- EquipMent[6]={}
+-- EquipMent[6][1]={type="xueZi",name="普通布靴",attack=0,defense=10}
+-- EquipMent[6][2]={type="xueZi",name="普通棉靴",attack=0,defense=15}
+-- EquipMent[6][3]={type="xueZi",name="普通皮靴",attack=0,defense=25}
+-- EquipMent[6][4]={type="xueZi",name="幽影布靴",attack=10,defens=35}
+-- EquipMent[6][5]={type="xueZi",name="奔雷靴",attack=25,defense=45}
